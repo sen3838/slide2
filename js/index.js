6d4a4d91f8e5627039356ba4bf4b8b755ee3bc4c
sen3838 <tpsk9972@naver.com>
2024-11-08 16:23:37 +0900

    title 수정

--- js/index.js
+++ js/index.js
@@ -2,12 +2,34 @@ $(function () {
   let total = $(".panel li").length;
   console.log(total);
   let i = 0;
+  slide();
   function slide() {
+    timer = setInterval(function () {
+      if (i == total - 1) {
+        $(".panel")
+          .stop()
+          .animate({ "margin-left": "-2000px" }, function () {
+            $(".panel").css({ "margin-left": 0 });
+          });
+        i = 0;
+      } else {
+        $(".panel")
+          .stop()
+          .animate({ "margin-left": -i * 500 });
+      }
+      $(".navi li").removeClass("on");
+      $(".navi li").eq(i).addClass("on");
+    }, 2000);
+  }
+
+  $(".next").on("click", function () {
+    clearInterval(timer);
+    i++;
     if (i == total - 1) {
       $(".panel")
         .stop()
         .animate({ "margin-left": "-2000px" }, function () {
-          $(".panel").css({ "margin-left": "0" });
+          $(".panel").css({ "margin-left": 0 });
         });
       i = 0;
     } else {
@@ -17,40 +39,30 @@ $(function () {
     }
     $(".navi li").removeClass("on");
     $(".navi li").eq(i).addClass("on");
-  }
-
-  timer = setInterval(function () {
-    i++;
     slide();
+  });
 
-    $(".next").on("click", function () {
-      clearInterval(timer);
-      if (i == total - 1) {
-        i = 0;
-      } else {
-        i++;
-      }
-      slide();
-    });
+  $(".prev").on("click", function () {
+    clearInterval(timer);
+    i--;
+    if (i < 0) {
+      $(".panel").css({ "margin-left": "-2000px" });
+      $(".panel").stop().animate({ "margin-left": "-1500px" });
+      i = 3;
+    } else {
+      $(".panel")
+        .stop()
+        .animate({ "margin-left": -i * 500 });
+    }
+    $(".navi li").removeClass("on");
+    $(".navi li").eq(i).addClass("on");
+    slide();
+  });
 
-    $(".prev").on("click", function () {
-      clearInterval(timer);
-      i--;
-      if (i < 0) {
-        $(".panel").css({ "margin-left": "-2000px" });
-        $(".panel").stop().animate({ "margin-left": "-1500px" });
-        i = 3;
-      } else {
-        $(".panel")
-          .stop()
-          .animate({ "margin-left": -i * 500 });
-      }
-      slide();
-    });
-
-    $(".navi li").on("click", function () {
-      i = $(this).index();
-      slide();
-    });
-  }, 1000);
+  $(".navi li").on("click", function () {
+    i = $(this).index();
+    $(".navi li").removeClass("on");
+    $(".navi li").eq(i).addClass("on");
+    slide();
+  });
 });
